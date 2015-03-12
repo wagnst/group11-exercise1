18193222dcdf74a35cc6854711d5953bd8f6cb62
wagnst <mail@steffenwagner.com>
2015-03-12 13:51:50 +0100

    implemented constructor for Vecotr

--- Kyrat/src/Vector.java
+++ Kyrat/src/Vector.java
@@ -3,5 +3,34 @@
  * Project: Kyrat
  */
 
-public class Vector {
+public class Vector extends java.lang.Object {
+
+    private double x,y,z;
+
+    /**
+     * Erzeugt einen Null-Vector, d.h. den Vektor bei dem alle Komponenten den Wert 0 haben.
+     *
+     * @author wagnst
+     */
+    public Vector() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+    }
+
+    /**
+     * Erzeugt einen neuen Vektor mit den angebgebenen Elementen.
+     *
+     * @param x die x-Komponente des Vektors
+     * @param y die y-Komponente des Vektors
+     * @param z die z-Komponente des Vektors
+     *
+     * @author wagnst
+     */
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
 }
